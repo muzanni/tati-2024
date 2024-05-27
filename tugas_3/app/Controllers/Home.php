57504dd30163c0a1f1b32e94c100ebbce59aa6ce
Muzanni <moezanni@gmail.com>
2024-05-27 15:28:52 +0700

    tugas 3 - logic

--- tugas_3/app/Controllers/Home.php
+++ tugas_3/app/Controllers/Home.php
@@ -6,8 +6,6 @@ class Home extends BaseController
 {
     public function index()
     {
-
-
         $url = 'https://aceh.tribunnews.com/tag/aceh-jaya';
 
         $html = file_get_contents($url);
@@ -24,6 +22,26 @@ class Home extends BaseController
 
         echo view('header');
         echo view('index', $data);
+        echo view('footer');
+    }
+
+    public function list(){
+         $url = 'https://aceh.tribunnews.com/tag/aceh-jaya';
+
+        $html = file_get_contents($url);
+        $pattern = '/<h3>(.*?)<\/h3>/s';
+        preg_match_all($pattern, $html, $matches);
+        $headings = $matches[1];
+        $data['data'] = $headings;
+
+        $url2 = 'https://aceh.tribunnews.com/tag/calang';
+        $html2 = file_get_contents($url2);
+        preg_match_all($pattern, $html2, $matches);
+        $headings2 = $matches[1];
+        $data['data2'] = $headings2;
+
+        echo view('header');
+        echo view('list', $data);
         echo view('footer');
     }
   
